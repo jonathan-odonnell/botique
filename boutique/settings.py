0ad418d0bc7fa0294feca259d9461fde0352e576
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2022-12-10 15:27:52 +0000

    Amended allowed hosts logic in settings.py

--- boutique/settings.py
+++ boutique/settings.py
@@ -27,8 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-store-7.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
